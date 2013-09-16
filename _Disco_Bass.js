45a07db8d64198b110b5d4eb75d2aadc8533533b
JuliusStarship <captain.julius.starship@gmail.com>
2013-09-16 10:01:33 -0400

    Initial commit of two midi plugins

---
+++ _Disco_Bass.js
@@ -0,0 +1,41 @@
+// _Disco_Bass
+
+NeedsTimingInfo = true;
+
+function HandleMIDI(event) {
+
+	event.send();
+
+	var info = GetTimingInfo();
+ // 	var speed = GetParameter("Speed"); 
+ // 	var articulation = GetParameter("Articulation"); 
+
+	var intervals = {
+		fifth: {
+			pitch_value: 8
+		}, 
+		octave_up: {
+			pitch_value: 12
+		}
+	};
+
+	if (event instanceof NoteOn) {
+	    var on = new NoteOn; //make a new note on
+	    on.pitch = event.pitch + 12; //set it's pitch to C3
+	    on.sendAfterBeats(.5); //send the note
+	    event.sendAfterBeats(1); //send the note
+	    on.sendAfterBeats(1.5); //send the note
+
+    }
+                               //it's pitch value (to C3)
+    if (event instanceof NoteOff) {
+    	var off = new NoteOff; //make a note off using the note on to initialize 
+	    off.pitch = event.pitch + 12;
+	    off.sendAfterBeats(.5); //send a note off one beat later                       
+	    event.sendAfterBeats(1); //send a note off one beat later                       
+	    off.sendAfterBeats(1.5); //send a note off one beat later                       
+    }
+
+}
+
+// var PluginParameters = [{name:"Speed", defaultValue:0, minValue:0, maxValue:1, numberOfSteps:4}, {name:"Articulation", type:"menu", valueStrings:["Staccato", "Legato"]}];
---
+++ _Major_Ninths.js
@@ -0,0 +1,47 @@
+// _Major_Ninths
+
+NeedsTimingInfo = true;
+
+function HandleMIDI(event) {
+
+	event.send();
+
+	var info = GetTimingInfo();
+ 	var speed = GetParameter("Speed"); 
+ 	var articulation = GetParameter("Articulation"); 
+
+	var intervals = {
+		major_third: {
+			pitch_value: 5,
+			delay: .25
+		}, 
+		major_seventh: {
+			pitch_value: 11,
+			delay: .50
+		},
+		ninth: {
+			pitch_value: 14,
+			delay: .75
+		}
+	};
+
+	if (event instanceof NoteOn) {
+		for (var key in intervals) {
+			intervals[key].on = new NoteOn;
+			intervals[key].on.pitch = event.pitch + intervals[key].pitch_value;
+			intervals[key].on.sendAfterBeats(speed * intervals[key].delay);
+		}
+	}
+
+	if (event instanceof NoteOff && articulation === 0) {
+		//MIDI.allNotesOff();
+		for (var key in intervals) {
+			intervals[key].off = new NoteOff;
+			intervals[key].off.pitch = event.pitch + intervals[key].pitch_value;
+			intervals[key].off.sendAfterBeats(speed * intervals[key].delay);
+		}
+	}
+
+}
+
+var PluginParameters = [{name:"Speed", defaultValue:0, minValue:0, maxValue:1, numberOfSteps:4}, {name:"Articulation", type:"menu", valueStrings:["Staccato", "Legato"]}];
